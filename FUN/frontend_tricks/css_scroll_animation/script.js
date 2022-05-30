d7749a3556725e8d8b80f4558d4972a60d40521f
Nikk-0x11 <nikhildhiman9050@gmail.com>
2022-05-31 02:58:53 +0530

    css scroll animation

---
+++ FUN/frontend_tricks/css_scroll_animation/script.js
@@ -0,0 +1,12 @@
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+  const triggerBotton = (window.innerHeight / 5) * 4
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+    if (boxTop < triggerBotton) box.classList.add("show")
+  })
+}
+
+window.addEventListener("scroll", checkBoxes)
+checkBoxes()
